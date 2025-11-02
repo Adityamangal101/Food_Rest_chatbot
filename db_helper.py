a2d4d7d7676a10823772acef452c50f770cfb776
Adityamangal101 <mangaladitya19@gmail.com>
2025-11-02 18:02:20 +0530

    Corecting DB config

--- db_helper.py
+++ db_helper.py
@@ -8,7 +8,7 @@ global conn
 # Read credentials from .env
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT", 3306)),
+    "port": int(os.getenv("DB_PORT")),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASS"),
     "database": os.getenv("DB_NAME"),
